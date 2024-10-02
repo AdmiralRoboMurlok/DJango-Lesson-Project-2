aa9ea24b76696bf112886d56641e716cf0a692fb
Gerard <gerard.dziwinski@infotech.edu.pl>
2024-10-02 13:54:20 +0200

    połowa zadania

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=120)
+    location = models.CharField(max_length=120)
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Employee
 
 # Create your views here.
+def index(request):
+    lista_employ = Employee.objects.all()
+
+    return render(request, 'index.html', context={
+        'elements': lista_employ
+    })
